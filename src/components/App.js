ed7d3890aff812ee02fee73bf2a1e7c909689440
Mohammad Amin Moeini <313mohamadamin83@gmail.com>
2021-09-14 16:02:42 +0430

    initial react project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+    return (
+        <React.StrictMode>
+            <h2>React</h2>
+        </React.StrictMode>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+
+
+const rootElement = document.getElementById('root');
+
+// this structure is : reactELement , React DOM , [function callback]
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </>,
+  rootElement
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//  in this example : 
+// first run componentWillMount () in Parent Component then run render method
+// then run componentWillMount () in Child Component  then render method
+// then run componentDidMount () in Child Component
+// and then run componentDidMount() in Parent Component
+
+
+// note : if mounting the components in DOM is done !  components is ready
+// and each component mounted : means inserted to DOM
+
+
+// Error Boundary
+// Context 
+// Portal
+// Refs   ==> refs use 2 way --> 1.React.createRef() and pass the ref to element ... 2. use for callbacks example in below: 
+// this.input = null;
+// this.callBack = (element) => { this.input = element }
+// pass the this.input for ref to element
+// now : use in componentDidMount for name : this.input
+// ---------
+
+// Forwarding Refs 
+
